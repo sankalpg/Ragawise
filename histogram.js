d74fcecc6ce4435cf16a39166be38e7644cdbfb8
swapnilgt <swapnilgupta.iiith@gmail.com>
2015-10-20 13:23:44 +0200

    Adding the files for graph plotting

--- histogram.js
+++ histogram.js
@@ -1,6 +1,6 @@
 //d3.select("body").append("p").text("New paragraph!");
 
-var CANVAS_WIDTH = 960;
+var CANVAS_WIDTH = 1300;
 var CANVAS_HEIGHT = 500;
 
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
@@ -10,9 +10,9 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
 var svg;
 var x; // scale variable for x
 var y; // scale variable for y
+var bar;
 
 var xAxis;
-
 var yAxis;
 
 /**
@@ -22,8 +22,15 @@ function initDraw(xRange, yRange) {
   svg = d3.select("body").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom)
   .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  x = d3.scale.linear().range([0, width]).domain([xRange.min, xRange.max]);
+  //x = d3.scale.linear().range([0, width]).domain([xRange.min, xRange.max]);
+  var xDomain = [];
+  for (var i = xRange.min; i <= xRange.max; i++) {
+    xDomain.push(i);
+  }
+  x = d3.scale.ordinal().domain(xDomain).rangeBands([0, width]);
+
   y = d3.scale.linear().range([height, 0]).domain([yRange.min,yRange.max]);
+  //y = d3.scale.linear().range([height, 0]);
 
   xAxis = d3.svg.axis()
     .scale(x)
@@ -46,9 +53,7 @@ function initDraw(xRange, yRange) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Cents");
-
-  svg.append("path").attr("id","graph");
+      .text("Prominence");
 }
 
 var line = d3.svg.line()
@@ -62,21 +67,18 @@ var line = d3.svg.line()
   });
 
 function draw(data) {
-
-  var testD = [];
-  //for (var i = 0; i <1200; i++) {
-    /*if (i < 300) {
-      testD.push(0);
-    } else if (i < 600){
-      testD.push(50);
-    } else if (i < 900) {
-      testD.push(100);
-    } else {
-      testD.push(150);
-    }*/
-    //testD.push(Math.random() * 150);
-  //}
-  console.log(Array.max(data));
- 
-  svg.select("path#graph").datum(data).attr("class", "line").attr("d", line);
+  var tempData = [];
+  for (var i = 0; i < 120; i++) {
+    tempData.push(Math.random() * 30);
+  }
+  console.log(tempData);
+  svg.selectAll(".bar").remove();
+  svg.selectAll(".bar")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d, i) {return x(i)})
+      .attr("y", function(d, i) {return y(d)})
+      .attr("width", x.rangeBand())
+      .attr("height", function(d) {return height - y(d)});
 }
