7b8748b73335b6398dfd7802b435876c8beca6d2
Alastair Porter <alastair.porter@upf.edu>
2017-11-21 16:34:48 +0100

    code/style cleanups

--- api/ragawise.py
+++ api/ragawise.py
@@ -1,19 +1,14 @@
 from __future__ import unicode_literals
-from flask import Flask, request, jsonify, current_app
-import requests
-import sys
-from functools import wraps
-import os.path
-import json
-from flask.ext.cors import CORS
+
 import codecs
+import json
+from functools import wraps
+
+from flask import Flask, request, jsonify, current_app
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
-@app.route('/')
-def index():
-    return "raga phrase demo"
 
 def support_jsonp(f):
     """Wraps JSONified output for JSONP"""
@@ -28,35 +23,28 @@ def support_jsonp(f):
 
     return decorated_function   
 
+
 @app.route('/get_raga_info', methods=['GET', 'POST'])
 @support_jsonp
 def get_raga_info():
-	"""
-	"""
-	#read a wav sound
-	raga_info = json.load(codecs.open('../data/raga_infos.json'))
-	return jsonify(**raga_info)    
+    raga_info = json.load(codecs.open('../data/raga_infos.json'))
+    return jsonify(**raga_info)
 
 
 @app.route('/get_raga_indexes', methods=['GET', 'POST'])
 @support_jsonp
 def get_raga_indexes():
-	"""
-	"""
-	#read a wav sound
-	raga_indexes = json.load(codecs.open('../data/raga_indexes.json'))
-	return jsonify(**raga_indexes) 	
+    raga_indexes = json.load(codecs.open('../data/raga_indexes.json'))
+    return jsonify(**raga_indexes)
+
 
 @app.route('/get_thaat_info', methods=['GET', 'POST'])
 @support_jsonp
 def get_thaat_info():
-    """
-    """
-    #read a wav sound
     thaat_info = json.load(codecs.open('../data/thaat_info.json'))
     return jsonify(**thaat_info)      
 
 
 if __name__ == '__main__':
     app.config['DEBUG'] = True
-    app.run(host= '0.0.0.0', port=5001, debug = True)        
+    app.run(host='0.0.0.0', port=5001, debug=True)
